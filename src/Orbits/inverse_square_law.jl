8f7aedbb5cafe5dc8a20aea66d257be1dab0012d
Cristóbal Armaza <ca455@cornell.edu>
2022-10-11 12:44:28 -0400

    Add function to compute total energy

--- src/Orbits/inverse_square_law.jl
+++ src/Orbits/inverse_square_law.jl
@@ -23,4 +23,22 @@ function acceleration(x::SVector{3, Float64})
     return -x / norm(x)^3
 end
 
+"""
+    energy(x, v)
+
+The total energy of a test particle orbiting a point mass at the origin.
+
+## Arguments
+
+  - `x::SVector{3, Float64}`: the Cartesian position of the test particle.
+  - `v::SVector{3, Float64}`: the Cartesian velocity of the test particle.
+
+## Returns
+
+  - `Float64`: the total energy.
+"""
+function energy(x::SVector{3, Float64}, v::SVector{3, Float64})
+    return 0.5 * norm(v)^2 - 1.0 / norm(x)
+end
+
 end
--- test/inverse_square_law.jl
+++ test/inverse_square_law.jl
@@ -15,9 +15,20 @@ function test()
         @testset "Acceleration" begin
             x = SVector{3, Float64}(Random.randn(rng, 3))
             acc = InverseSquareLaw.acceleration(x)
-            acc_expected = -1.0 * x / (x[1]^2 + x[2]^2 + x[3]^2)^1.5
 
+            acc_expected = -1.0 * x / (x[1]^2 + x[2]^2 + x[3]^2)^1.5
             @test isapprox(acc, acc_expected)
+        end
+
+        @testset "Energy" begin
+            x = SVector{3, Float64}(Random.randn(rng, 3))
+            v = SVector{3, Float64}(Random.randn(rng, 3))
+            e = InverseSquareLaw.energy(x, v)
+
+            e_expected =
+                0.5 * (v[1]^2 + v[2]^2 + v[3]^2) -
+                1.0 / sqrt(x[1]^2 + x[2]^2 + x[3]^2)
+            @test isapprox(e, e_expected)
         end
     end
 end
