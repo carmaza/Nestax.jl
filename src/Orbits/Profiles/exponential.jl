0666f6379d27793720f412f0cce0ed95f0202cf0
Cristóbal Armaza <ca455@cornell.edu>
2022-10-17 11:14:10 -0400

    Add missing dox for number_fraction!

--- src/Orbits/Profiles/exponential.jl
+++ src/Orbits/Profiles/exponential.jl
@@ -3,6 +3,18 @@
 
 module Exponential
 
+"""
+    number_fraction!(frac, r)
+
+For a spherical distribution of N particles, this function computes f(r),
+defined such that there are n = N * (f(r1) - f(r2)) particles in the shell
+with bounds r in [r1, r2].
+
+## Arguments
+
+    - `frac::Vector{Float64}`: the fraction of particles.
+    - `r::Vector{Float64}`: any bound(s) within which to compute the fraction.
+"""
 function number_fraction!(frac::Vector{Float64}, r::Vector{Float64})
     @. frac[:] = exp(-2.0 * r) * (1.0 + 2.0 * r * (1.0 + r))
 end
