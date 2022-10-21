d32851734116b73fe42a8abaf5a3f0a8c3fce319
Cristóbal Armaza <ca455@cornell.edu>
2022-10-20 22:18:46 -0400

    Make Distributions a module

---
+++ src/Orbits/Distributions/Distributions.jl
@@ -0,0 +1,8 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+
+module Distributions
+
+include("./generalized_spiral.jl")
+
+end
--- test/generalized_spiral.jl
+++ test/generalized_spiral.jl
@@ -4,7 +4,7 @@
 using Random
 using Test
 
-include("../src/Orbits/Distributions/generalized_spiral.jl")
+include("../src/Orbits/Distributions/Distributions.jl")
 
 function test()
     seed = Random.rand(1:(10^10))
@@ -14,13 +14,13 @@ function test()
         N = UInt64(Random.rand(rng, 2:10))
 
         h = Vector{Float64}(undef, N)
-        GeneralizedSpiral._h!(h)
+        Distributions.GeneralizedSpiral._h!(h)
 
         @testset verbose = true "Functions h and phi" begin
             h_expected = [-1.0 + 2.0 * (l - 1.0) / (N - 1.0) for l in 1:N]
             @test isapprox(h, h_expected)
 
-            phase = GeneralizedSpiral._phase(h[1], N)
+            phase = Distributions.GeneralizedSpiral._phase(h[1], N)
             phase_expected = 3.6 / sqrt(N * (1.0 - h[1]^2))
 
             @test isapprox(phase, phase_expected)
@@ -29,14 +29,15 @@ function test()
         @testset verbose = true "Set angles" begin
             theta = Vector{Float64}(undef, N)
             phi = Vector{Float64}(undef, N)
-            GeneralizedSpiral.set_angles!(theta, phi)
+            Distributions.GeneralizedSpiral.set_angles!(theta, phi)
 
             theta_expected = [acos(hl) for hl in h]
             phi_expected = zeros(N)
 
             for l in 2:(N - 1)
                 phi_expected[l] =
-                    phi_expected[l - 1] + GeneralizedSpiral._phase(h[l], N)
+                    phi_expected[l - 1] +
+                    Distributions.GeneralizedSpiral._phase(h[l], N)
             end
 
             @test isapprox(theta, theta_expected)
