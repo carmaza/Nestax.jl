f82008a6c6d4e6c5c9dce667a4676dec2cd8af22
Cristóbal Armaza <ca455@cornell.edu>
2022-10-29 18:17:54 -0400

    Add initial data setting

---
+++ src/Orbits/initial_data.jl
@@ -0,0 +1,80 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+
+module InitialData
+
+using Random
+using StaticArrays
+
+include("./euclidean.jl")
+include("./shell.jl")
+
+function set!(
+    x::Vector{SVector{3, Float64}},
+    v::Vector{SVector{3, Float64}},
+    clump,
+    Nshells,
+    write = true
+)
+    N = clump.N % UInt64
+    dr = clump.profile.r90() / Nshells
+    bounds = [dr * k for k in 0:Nshells]
+    numbers = Vector{Int}(undef, Nshells)
+    Shell.particle_number!(numbers, clump.profile, N, bounds)
+
+    # Assign radial coordinate of each shell to outer bound.
+    # TO-DO: maybe assign bounds average instead?
+    popfirst!(bounds) # Delete origin.
+    R = 40.0 * sqrt(clump.gamma)
+    for k in eachindex(bounds)
+        bounds[k] *= R
+    end
+
+    Neff = sum(numbers)
+    for i in 1:Neff
+        push!(x, clump.position)
+    end
+
+    seed = Random.rand(1:(10^10))
+    rng = Random.Xoshiro(seed)
+    shift = 0
+    for k in eachindex(numbers)
+        n = numbers[k]
+        if n > 0
+            theta = Vector{Float64}(undef, n)
+            phi = Vector{Float64}(undef, n)
+            clump.distribution.set_angles!(theta, phi)
+
+            axis = SVector{3}(Random.randn(rng, 3))
+            angle = Random.randn(rng)
+            for j in 1:n
+                ind = j + shift
+                x[ind] += Euclidean.rotate_about(
+                    Euclidean.cartesian_from_spherical(
+                        bounds[k],
+                        theta[j],
+                        phi[j]
+                    ),
+                    axis,
+                    angle
+                )
+            end
+        end
+        shift += n
+    end
+
+    for i in 1:Neff
+        push!(v, clump.velocity)
+    end
+
+    if write
+        initial = open("initial.dat", "w+")
+        for i in 1:Neff
+            println(initial, x[i][1], " ", x[i][2], " ", x[i][3])
+        end
+        close(initial)
+    end
+    return nothing
+end
+
+end
