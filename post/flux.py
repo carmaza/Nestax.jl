9572ca6be8db312ce9224dc9c7fbedc756f9d300
Cristóbal Armaza <ca455@cornell.edu>
2022-11-30 15:40:58 -0500

    PostProcessing: add flux

---
+++ post/flux.py
@@ -0,0 +1,27 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+
+from particle import Particle
+from chrono import Interval
+
+
+class Flux:
+
+    def __init__(self, time_interval):
+        self._time_interval = time_interval
+        self._particles = []
+
+    def add_particle(self, particle):
+        self._particles.append(particle)
+
+    @property
+    def particles(self):
+        return self._particles
+
+    @property
+    def particle_number(self):
+        return len(self._particles)
+
+    @property
+    def time_interval(self):
+        return self._time_interval
