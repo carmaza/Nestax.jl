acf5dcf9f7cffc26c0be2d6e26e8afcffc6efecc
Cristóbal Armaza <ca455@cornell.edu>
2022-12-13 00:17:58 -0500

    Cleanup and document Axion

--- post/axion.py
+++ post/axion.py
@@ -6,16 +6,55 @@ import numpy as np
 
 
 class Axion:
+    """
+    Functions related to the QCD axion.
 
-    def mass_or_decay(self, md):
+    """
+
+    def mass_or_decay_constant(self, f_or_m, lambda_sqrd=(5.7 * 1.e6)):
         """
-        fa in GeV, mass in ueV.
+        Assume a relation lambda_sqrd = mass * decay_constant to compute either
+        the mass of the decay constant.
+
+        The units are such that the decay constant is in GeV, and the mass in 
+        eV.
+ 
+        Parameters
+        ----------
+        `f_or_m`: array_like
+        Either the decay constant or the mass
+
+        `lambda_sqrd`: array_like (default: 5.7e12 GeV^2).
+        The Lambda QCD parameter squared.
+     
+        Returns
+        -------
+        out : array_like
+        If `f_or_m` is the mass (decay constant), return the decay constant
+        (mass).
+        
         """
-        return 5.7 * 1.e12 / md
+        return lambda_sqrd / f_or_m
 
     def photon_coupling(self, fa, e_over_n):
         """
-        fa in GeV. Result given in inverse GeV.
+        The axion-photon coupling constant in QCD.
+
+        Parameters
+        ----------
+        
+        `fa` : array_like
+        The decay constant.
+
+        `e_over_n` : array_like
+        The electromagnetic-to-color anomaly parameter of the particular model.
+
+
+        Returns
+        -------
+
+        out : array_like
+        The coupling constant, in units of inverse decay constant.
         """
         return (0.5 * astro.alpha.value / np.pi) * np.absolute(e_over_n -
                                                                1.92) / fa
