26583dac7d59103a55ba3d3889d27b30445e7f86
Cristóbal Armaza <ca455@cornell.edu>
2022-12-13 00:23:02 -0500

    Cleanup and document Time functions

--- post/chrono.py
+++ post/chrono.py
@@ -3,6 +3,19 @@
 
 
 class Time:
+    """
+    A time instant.
+
+    Parameters
+    ---------
+    
+    `time_id` : string
+    A unique ID.
+
+    `value` : float
+    The numeric value of the time instant.
+
+    """
 
     def __init__(self, time_id, value):
         self._time_id = time_id
@@ -18,13 +31,38 @@ class Time:
 
 
 class Interval:
+    """
+    A time interval.
+
+    Parameters
+    ----------
+
+    `extents` : tuple
+    A 2-tuple containing the initial and final `Time` defining the interval.
+    """
 
     def __init__(self, extents):
         self._extents = extents
 
-    def within_extents(self, time):
-        return time.value >= self._extents[0] and time.value < self._extents[1]
-
     @property
     def extents(self):
         return self._extents
+
+    def within_extents(self, time):
+        """
+        Whether the given `time` value is contained in this interval.
+
+        Parameters
+        ----------
+
+        `time` : obj
+        The `Time` object whose value is being tested.
+
+        Returns
+        -------
+
+        out : bool
+        Whether the value of `time` lies in this time interval.
+
+        """
+        return time.value >= self._extents[0] and time.value < self._extents[1]
